0859ef57a467d1dc6b0850687994ad56bc1fc221
Simon Schröter <simon.schroeter@gmail.com>
2016-03-03 23:53:35 +0100

    fix Http404 not found

--- questions/views.py
+++ questions/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def question(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
+    question = get_object_or_404(Question, pk=question_id)
     answers = question.answer_set.all()
 
-    return render(request, "question.html", {"question": question, "answers": answers})
+    return render(request, "question.html", {
+        "question": question, "answers": answers
+    })
