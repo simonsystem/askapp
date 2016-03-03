7bc958fb6c320084ebff56fddb428ae3ed83e134
Simon Schröter <simon.schroeter@gmail.com>
2016-03-04 00:51:06 +0100

    tests added

--- questions/tests.py
+++ questions/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Question, Answer
 
-# Create your tests here.
+class QuestionsTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(text="Wer hat an der Uhr gedreht?")
+        Question.objects.create(text="Wie spät ist es?")
+
+    def test_questions_can_be_answered(self):
+        """Animals that can speak are correctly identified"""
+        uhr = Question.objects.get(text="Wer hat an der Uhr gedreht?")
+        uhr.answer_set.create(text="Ich.")
+        uhr.save()
+    def test_answers_can_be_accepted(self):
+        """Animals that can speak are correctly identified"""
+        uhr = Question.objects.get(text="Wer hat an der Uhr gedreht?")
+        uhr.answer_set.create(text="Niemand.")
+        uhr.answer_set.get(text="Niemand.").is_accepted = True
+        uhr.save()
